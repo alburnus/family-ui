607be3810179b1c084c7ee680b3e764527874124
Krzysztof <k.ukleja@gmail.com>
2020-09-11 23:19:29 +0200

    First details page with some styles.

--- src/components/FamilyMember/MemberDetails.js
+++ src/components/FamilyMember/MemberDetails.js
@@ -1,11 +1,37 @@
 import React from 'react';
+import classes from './MemberDetails.css'
+import userImages from '../../assets/user.png';
+import bloodImage from '../../assets/blood-pressure.png';
+import triumphImage from '../../assets/triumph.png';
 
 export const MemberDetails = (props) => {
     return (
-        <React.Fragment>
-            MemberDetails View
-            {props.nickname}
-        </React.Fragment>
+        <div>
+            <h4 style={{padding: '15px'}}>Member Details</h4>
+            <div className={classes.main}>
+                <div className={classes.card}>
+                    <img src={userImages} alt="Avatar" style={{width: '100%'}}/>
+                    <div className={classes.container}>
+                        <h4><b>Personal Data</b></h4>
+                        <p>nickname: anonymous</p>
+                    </div>
+                </div>
+                <div className={classes.card}>
+                    <img src={bloodImage} alt="Avatar" style={{width: '100%'}}/>
+                    <div className={classes.container}>
+                        <h4><b>Blood pressure</b></h4>
+                        <p>Last: 120/80 71</p>
+                    </div>
+                </div>
+                <div className={classes.card}>
+                    <img src={triumphImage} alt="Avatar" style={{width: '100%'}}/>
+                    <div className={classes.container}>
+                        <h4><b>Body weight</b></h4>
+                        <p>Last: 92kg</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 };
 
